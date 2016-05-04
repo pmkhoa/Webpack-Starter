a93e01af32c25f6a18f4e8022ab8b436ef79714a
Khoa Pham <khoa.pham@me.com>
2016-05-03 22:58:04 -0700

    Add sample module

--- js/app.js
+++ js/app.js
@@ -2,4 +2,19 @@ import '../scss/app.scss';
 import $ from "jquery";
 import 'waypoints';
 
+import * as math from "./math";
 console.log('Webpack boilerplate starting...');
+
+class WebpackIntro {
+    constructor() {
+        this.initialize();
+    }
+
+    initialize() {
+        console.log("initialize");
+        console.log("2π = " + math.sum(math.pi, math.pi));
+    }
+}
+
+let webpackIntro = new WebpackIntro();
+
